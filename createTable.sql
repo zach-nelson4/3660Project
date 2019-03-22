fb6aed104eda5ccc88418a77c0c40e839cfdd083
muna2720 <amanda.munden@uleth.ca>
2019-03-22 16:43:12 -0600

    car table created

--- createTable.sql
+++ createTable.sql
@@ -1,1 +1,12 @@
-create table
+create table CARS
+(
+    CarID int,
+    ModName varchar(10),
+    EdName char(2),
+    Year int,
+    IntCol varchar(25),
+    ExtCol varchar(25),
+    VIN varchar(17),
+    primary key (CarID)
+);
+
