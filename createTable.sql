ff2d04e9578f542d541fcb0876552e086208cb42
muna2720 <amanda.munden@uleth.ca>
2019-04-01 17:13:36 -0600

    changes to createTable to get it to work on the database

--- createTable.sql
+++ createTable.sql
@@ -1,7 +1,7 @@
 create table cars
 (
     CarID int,
-    New/Used bool,
+    New_or_Used varchar(5),
     ModName varchar(10),
     EdName char(2),
     Year int,
@@ -29,33 +29,34 @@ create table oldcarpurchase
 (
 	OldCarPurchID int,
 	CarID int,
-	BookP float,
-	PricePaid float,
-	MilesUsed float,
-	Seller/DealerName char(15),
-	Location varchar(20),
+	BookP float(2),
+	PricePaid float(2),
+	MilesUsed float(2),
+	Seller_or_DealerName varchar(20),
+	Locations varchar(20),
 	primary key (OldCarPurchID),
-	foreign key (CarID)
+	foreign key (CarID) REFERENCES cars(CarID)
 );
 
 create table newcarpurchase
 (
 	NewCarPurchID int,
 	CarID int,
-	PurchP float,
-	ExpMiles float,
+	PurchP float(2),
+	ExpMiles float(2),
 	primary key (NewCarPurchID),
-	foreign key (CarID)
+	foreign key (CarID) REFERENCES cars(CarID)
 );
 
 create table repair
 (
 	CarID int,
-	EstCost float,
-	ActualCost float,
+	EstCost float(2),
+	ActualCost float(2),
 	Problem varchar(50),
-	RepairID int
-	foreign key (CarID)
+	RepairID int,
+	primary key (RepairID),
+	foreign key (CarID) REFERENCES cars(CarID)
 );
 
 
@@ -63,12 +64,12 @@ create table salestrans
 (
 	SalesTransID int,
 	CarID int,
-	SellingP float,
-	DownPmt float,
-	FinancedAmt float,
-	InterestRate float,
+	SellingP float(2),
+	DownPmt float(2),
+	FinancedAmt float(2),
+	InterestRate float(2),
 	primary key (SalesTransID),
-	foreign key (CarID)
+	foreign key (CarID) REFERENCES car(CarID)
 );
 
 create table employee
@@ -77,21 +78,22 @@ create table employee
 	fName char(20),
 	lName char(20),
 	Phone char(14),
-	Commission float,
+	Commission float(2),
+	SalesTransID int,
 	primary key (EmpID),
-	foreign key (SalesTransID)
+	foreign key (SalesTransID) REFERENCES salestrans(SalesTransID)
 );
 
 create table warranty
 (
 	SalesTransID int,
-	Type varchar(20),
-	Deductible float,
+	Type_of_warranty varchar(20),
+	Deductible float(2),
 	StartDate date,
 	ItemsCovd varchar(50),
-	Length varchar(40),
-	TotalCost float,
-	MonthlyCost float,
+	Duration varchar(40),
+	TotalCost float(2),
+	MonthlyCost float(2),
 	WarrID int,
 	primary key (SalesTransID)
 );
@@ -103,7 +105,7 @@ create table employerhist
 	Title char(10),
 	Supervisor varchar(20),
 	Phone char(14),
-	Address varchar(50),
+	Home_Address varchar(50),
 	StartDate date,
 	EmpHistID int,
 	primary key (CustID)
@@ -113,11 +115,11 @@ create table payment
 (
 	CustID int,
 	NoPmt int,
-	Amount float,
+	Amount float(2),
 	StartDate date,
 	DueDate char(7),
-	NoDaysLate float,
-	AvgDaysLate float,
+	NoDaysLate float(2),
+	AvgDaysLate float(2),
 	BankAcct int,
 	Cosigner char(20),
 	PmtID int,
