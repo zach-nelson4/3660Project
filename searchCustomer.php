88f3e47a3a89df040c5ae8e81b7a5f6ad545f5c2
muna2720 <amanda.munden@uleth.ca>
2019-04-02 18:38:08 -0600

    fixing a bug

--- searchCustomer.php
+++ searchCustomer.php
@@ -41,7 +41,7 @@
 	if( mysqli_num_rows($result) > 0){
 		while($row = mysqli_fetch_assoc($result))
 		{
-			echo "<tr>"."<td>" . $row["custID"] . "</td>" .
+			echo "<tr>"."<td>" . $row["CustID"] . "</td>" .
 			"<td>" . $row["fName"] . "</td>" .
 			"<td>" . $row["lName"] . "</td>" .		
 			"<td>" . $row["streetName"] . "</td>" .
