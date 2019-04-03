da9a5778598790d1f73923f10497455cce4e73c9
muna2720 <amanda.munden@uleth.ca>
2019-04-03 00:25:49 -0600

    creating employee search

---
+++ searchEmployees.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel = "stylesheet" href = "css/main-style.css">
+    </head> 
+
+    <body>
+        <br><center><h3> Results </h3></center><br>
+
+        <table id = "tables">
+            <tr>
+                <th>
