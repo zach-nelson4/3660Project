9950431dee9a3476db099af0d5678bdcc8651786
zach-nelson4 <48226189+zach-nelson4@users.noreply.github.com>
2019-04-03 19:50:17 -0600

    Fixing Selling car

--- carSellingSubmit.php
+++ carSellingSubmit.php
@@ -15,14 +15,42 @@
 	$carID = mysqli_real_escape_string($conn, $_POST['holdCarID']);
 	$sellP = mysqli_real_escape_string($conn, $_POST['holdsellP']);
 	$downP = mysqli_real_escape_string($conn, $_POST['holdDownP']);
-	$financeAmt = mysqli_real_escape_string($conn, $_POST['holdFin']);
 	$IntR = mysqli_real_escape_string($conn, $_POST['holdInt']);
 	$date = mysqli_real_escape_string($conn, $_POST['holdDate']);
+	$commis = mysqli_real_escape_string($conn, $_POST['Commis']);
+	$empID = mysqli_real_escape_string($conn, $_POST['empID']);
+	$warranty = mysqli_real_escape_string($conn, $_POST['warranty']);
+	$deduct = mysqli_real_escape_string($conn, $_POST['Deduct']);
+	$durat = mysqli_real_escape_string($conn, $_POST['Durat']);
+	$totalC = mysqli_real_escape_string($conn, $_POST['TotalC']);
+	$monthC = mysqli_real_escape_string($conn, $_POST['MonthC']);
 
-	$sql = "INSERT INTO salestrans VALUES(NULL, '$carID', '$sellP', '$downP', '$financeAmt', '$IntR', '$date')";
+	$sql = "INSERT INTO salestrans VALUES(NULL, '$carID', '$sellP', '$downP', ('$sellP'-'$downP'), '$IntR', '$date')";
 	$retval = mysqli_query($conn, $sql);
 
 	$sql = "DELETE FROM cars WHERE CarID = '$carID'";
+	$retval = mysqli_query($conn, $sql);
+
+	$sql = "UPDATE employee SET Commission = Commission + '$commis' WHERE EmpID = '$empID'";
+	$retval = mysqli_query($conn, $sql);
+
+	$sql = "SELECT SalesTransID from salestrans WHERE (CarID = '$carID')";
+	$retval = mysqli_query($conn, $sql);
+	while($row = $retval->fetch_assoc()){
+		$salesID = $row['SalesTransID'];
+	}
+
+	echo $salesID;
+	echo $warranty;
+	echo $deduct;
+	echo $date;
+	echo $carID;
+	echo $durat;
+	echo $totalC;
+	echo $monthC;
+
+	$sql = "INSERT INTO warranty VALUES('$salesID','$warranty','$deduct','$date','$carID','$durat','$totalC','$monthC',NULL)";
+	echo $sql;
 	$retval = mysqli_query($conn, $sql);
 
 	$url = "http://localhost/3660Project/index.html";
