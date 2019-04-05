192c2fcdb8acdbb47d11ea32622f20637be0f812
zach-nelson4 <48226189+zach-nelson4@users.noreply.github.com>
2019-04-04 22:32:44 -0600

    Add files via upload

--- UpdateCustomer.php
+++ UpdateCustomer.php
@@ -10,27 +10,51 @@
 
 	if (isset($_POST['customer_search_submit']))
 	{
+	$custID = 0;
+	$fName = $lName = $Dob = $gender = $add = $city = $prov = $postCode = $phone = "";
+
 	$custID = mysqli_real_escape_string($conn, $_POST['custID']);
 
-?>	
+    	$sql = "SELECT * FROM customer WHERE (CustID = '$custID')";
+        $result = mysqli_query($conn, $sql);
+        if(! $result)
+        die('could not get data: ' .mysqli_error($conn));
+            while($row = mysqli_fetch_assoc($result))
+            {
+                $fname = $row['fName'];
+                $lname = $row['lName'];
+     		$Dob = $row['DOB'];
+                $gender = $row['gender'];
+     		$add = $row['streetName'];
+                $city = $row['city'];
+     		$prov = $row['province'];
+                $postCode = $row['postCode'];
+   		$phone = $row['phone'];
 
-   <body>
-    <center><h2>Update Customer Information</h2></center>
-    <br><br>
-    Please enter the new information: 
-    <br><br>
-	Customer ID: $custID;
-    <form action="updateCustomer.php" method="post" style="overflow-x:auto;">
-        First Name: <input type="string" name="fName" required ><br><br>
-        Last Name: <input type="string" name="lName" required ><br><br>
-        Date of birth: <input type="string" name="dob" required><br><br>
-        Gender: <input type ="string" name = "gender" maxlength = "1"> <br><br>
-        Address: <input type="string" name="streetName" ><br><br>
-        City: <input type="string" name="city" ><br><br>
-        Province: <input type="string" name="province" ><br><br>
-        Postal Code: <input type="string" name="postCode" required><br><br>
-        Phone Number: <input type="string" name="phone" required ><br><br>
-	<input type='hidden' name='custID' value= $custID">
+            } 
+    	}
+
+	
+
+   echo "<body>";
+   echo "<center><h2>Update Customer Information</h2></center>";
+   echo "<br><br>";
+   echo "Please enter the new information: ";
+   echo "<br><br>";
+	echo "Customer ID: " .$custID;
+    echo "<form action='cUpdateSubmit.php' method='post' style='overflow-x:auto;'>";
+    echo "First Name: <input type='string' name='fName' value=$fname ><br><br>"
+    echo    "Last Name: <input type='string' name='lName' value = $lname><br><br>"
+    echo    "Date of birth: <input type='string' name='dob' value=$Dob><br><br>"
+    echo    "Gender: <input type ='string' name = 'gender' maxlength = '1' value=$gender> <br><br>"
+    echo    "Address: <input type='string' name='streetName'value=$add ><br><br>"
+    echo    "City: <input type='string' name='city' value=$city ><br><br>"
+    echo    "Province: <input type='string' name='province' value=$prov ><br><br>"
+    echo    "Postal Code: <input type='string' name='postCode' value=$postCode><br><br>"
+    echo   "Phone Number: <input type='string' name='phone' value=$phone ><br><br>"
+	echo	"<input type='hidden' name='custID' value= $custID>"
+
+?>
         <br><br>
         <input type="submit" name="update_cust_submit">
         </form>
